a5baccb00f37d56cd1ec2329b0ded566e96605fb
Yiroma <yiromaric@gmail.com>
2025-03-05 15:47:51 +0100

    added logic post setup

--- api/src/repos/repos.controller.ts
+++ api/src/repos/repos.controller.ts
@@ -22,8 +22,9 @@ repos.get("/:reposid", (req: Request, res: Response) => {
 
 // POST route to insert a new repo
 repos.post("/", (req: Request, res: Response) => {
-  console.log(req.body);
-  res.status(200).send("Tout est ok pour l'ajout d'un repo");
+  const newId = Math.ceil(Math.random() * 1000000).toString();
+  data.push({ id: newId, ...req.body });
+  res.status(201).json({ message: "Repo created successfully", id: newId });
 });
 
 export default repos;
