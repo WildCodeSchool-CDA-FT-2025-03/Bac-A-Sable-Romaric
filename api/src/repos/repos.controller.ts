595561694f04042c60d21e15d94881e6cbe243b1
Yiroma <yiromaric@gmail.com>
2025-03-05 12:09:00 +0100

    setup route repos/:id

--- api/src/repos/repos.controller.ts
+++ api/src/repos/repos.controller.ts
@@ -3,8 +3,15 @@ import data from "../../data.json";
 
 const repos = express.Router();
 
+// GET route to recover all repos
 repos.get("/", (_, res: Response) => {
   res.status(200).json(data);
+});
+
+// GET route to recover a repo by id
+repos.get("/:reposid", (req, res) => {
+  const repo = data.find((rep) => rep.id === req.params.reposid);
+  res.status(200).json(repo);
 });
 
 export default repos;
