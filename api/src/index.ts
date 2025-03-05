fe621cfea72334b314fa0d0758ecd4fd9ef4b12d
Yiroma <yiromaric@gmail.com>
2025-03-05 11:50:30 +0100

    router setup, root route and check

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import "dotenv/config";
+import router from "./router";
 
 const app = express();
 
 const PORT = process.env.EXPRESS_SERVER_PORT || 3000;
+
+app.use("/api", router);
 
 app.listen(PORT, () => {
   console.info(`Server is running on port http://localhost:${PORT}`);
