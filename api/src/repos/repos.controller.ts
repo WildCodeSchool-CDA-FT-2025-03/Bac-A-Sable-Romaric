daa0bc568734b2acf7b9794ab46b988eee9340d7
Yiroma <yiromaric@gmail.com>
2025-03-05 15:55:13 +0100

    setup middleware to validate post repo data

--- api/src/repos/repos.controller.ts
+++ api/src/repos/repos.controller.ts
@@ -1,8 +1,33 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
+import Joi from "joi";
 import data from "../../data.json";
 import { Repos } from "./repos.types";
 
 const repos = express.Router();
+
+// Schema to validate the data of the repo
+const schema = Joi.object({
+  url: Joi.string().required(),
+  isPrivate: Joi.boolean().required(),
+  languages: Joi.array().items(
+    Joi.object({
+      size: Joi.number().required(),
+      node: Joi.object({
+        name: Joi.string(),
+      }),
+    })
+  ),
+});
+
+// Middleware to validate the data of the repo
+const validateRepo = (req: Request, res: Response, next: NextFunction) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+    res.status(422).json({ errorMessage: error.message });
+  } else {
+    next();
+  }
+};
 
 // GET route to recover all repos
 repos.get("/", (_, res: Response) => {
@@ -21,7 +46,7 @@ repos.get("/:reposid", (req: Request, res: Response) => {
 });
 
 // POST route to insert a new repo
-repos.post("/", (req: Request, res: Response) => {
+repos.post("/", validateRepo, (req: Request, res: Response) => {
   const newId = Math.ceil(Math.random() * 1000000).toString();
   data.push({ id: newId, ...req.body });
   res.status(201).json({ message: "Repo created successfully", id: newId });
