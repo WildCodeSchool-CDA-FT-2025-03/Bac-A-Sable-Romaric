b36c54dbd3415edf3340bc66064fd0835e920d8a
Yiroma <yiromaric@gmail.com>
2025-03-05 16:02:59 +0100

    project structure with validation layer

--- api/src/repos/repos.controller.ts
+++ api/src/repos/repos.controller.ts
@@ -1,33 +1,9 @@
-import express, { Request, Response, NextFunction } from "express";
-import Joi from "joi";
+import express, { Request, Response } from "express";
+import validateRepo from "./repos.validator";
 import data from "../../data.json";
 import { Repos } from "./repos.types";
 
 const repos = express.Router();
-
-// Schema to validate the data of the repo
-const schema = Joi.object({
-  url: Joi.string().required(),
-  isPrivate: Joi.boolean().required(),
-  languages: Joi.array().items(
-    Joi.object({
-      size: Joi.number().required(),
-      node: Joi.object({
-        name: Joi.string(),
-      }),
-    })
-  ),
-});
-
-// Middleware to validate the data of the repo
-const validateRepo = (req: Request, res: Response, next: NextFunction) => {
-  const { error } = schema.validate(req.body);
-  if (error) {
-    res.status(422).json({ errorMessage: error.message });
-  } else {
-    next();
-  }
-};
 
 // GET route to recover all repos
 repos.get("/", (_, res: Response) => {
