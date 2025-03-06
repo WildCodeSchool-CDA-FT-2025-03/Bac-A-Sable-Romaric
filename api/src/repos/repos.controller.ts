9d7c809cb74d7521bf6f4ff8b709d444d0155a47
Yiroma <yiromaric@gmail.com>
2025-03-06 11:32:36 +0100

    setup query params to select fields with map & reduce

--- api/src/repos/repos.controller.ts
+++ api/src/repos/repos.controller.ts
@@ -1,26 +1,39 @@
 import express, { Request, Response } from "express";
 import validateRepo from "./repos.validator";
 import data from "../../data.json";
-import { Repos } from "./repos.types";
+import { Repos, Fields } from "./repos.types";
 
 const repos = express.Router();
 
-// GET route to recover all repos
+// GET route to hit all repos
 repos.get("/", (req: Request, res: Response) => {
-  console.log("Hit all repo controller");
-  console.log(req.query);
-
+  // Filter by isPrivate
   let result = req.query.isPrivate
     ? data.filter((rep) => rep.isPrivate.toString() === req.query.isPrivate)
     : data;
 
-  if (req.query.limit) {
+  // Filter by limit
+  if (req.query.limit && result.length > +req.query.limit) {
     result = result.slice(0, +req.query.limit);
   }
+
+  // Filter by fields
+  if (req.query.fields) {
+    const fields = typeof req.query.fields === "string" ? req.query.fields.split(",") : [];
+
+    // Map the result to the fields
+    result = result.map((el: Repos) => {
+      // Map return array [res, res, res, res]
+      const res = fields.reduce((acc, field) => ({ ...acc, [field]: el[field] }), {}); // Array method return acc (string, number, object, array)
+
+      return res; // {fields[0]: ..., fields[1]: ..., fields[2]: ...}
+    }) as Repos[];
+  }
+
   res.status(200).json(result);
 });
 
-// GET route to recover a repo by id
+// GET route to hit a repo by id
 repos.get("/:reposid", (req: Request, res: Response) => {
   const repo = data.find((rep) => rep.id === req.params.reposid) as Repos;
 
