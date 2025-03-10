ad2c00e2bb94343fe9333c959c8356e36591095a
Yiroma <yiromaric@gmail.com>
2025-03-10 12:09:03 +0100

    install axios & init it to services/api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import type { Repos } from "./types/repoas.type";
+import api from "./services/api";
 
 import "./App.css";
 
@@ -7,10 +8,14 @@ function App() {
   const [data, setData] = useState<Repos[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:3001/api/repos")
-      .then((res) => res.json())
-      .then((repos) => setData(repos as Repos[]))
-      .catch((err) => console.error(err));
+    api
+      .get("/repos")
+      .then((repos) => {
+        setData(repos.data as Repos[]);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }, []);
 
   return (
