e76c2c308e4beb7f5f1308f52e744505c8df668d
Yiroma <yiromaric@gmail.com>
2025-03-10 12:14:11 +0100

    edit and setup custom repo data hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,9 @@
-import { useEffect, useState } from "react";
-import type { Repos } from "./types/repoas.type";
-import api from "./services/api";
+import useRepos from "./services/useRepos";
 
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState<Repos[]>([]);
-
-  useEffect(() => {
-    api
-      .get("/repos")
-      .then((repos) => {
-        setData(repos.data as Repos[]);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+  const { data } = useRepos();
 
   return (
     <>
