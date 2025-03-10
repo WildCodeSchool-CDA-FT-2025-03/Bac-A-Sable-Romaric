905660ffd5884ea3428925380c31e82e3f48e240
Yiroma <yiromaric@gmail.com>
2025-03-10 11:45:47 +0100

    install cors and init into api

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import router from "./router";
+import cors from "cors";
 
 const app = express();
 
 const PORT = process.env.EXPRESS_SERVER_PORT || 3000;
+
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use(express.json());
 app.use("/api", router);
