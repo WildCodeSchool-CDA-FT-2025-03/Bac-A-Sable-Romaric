5f3ca1c60acaa024c57f9460ffb8864d0243f36b
Yiroma <yiromaric@gmail.com>
2025-03-11 21:27:04 +0100

    create footer component

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import { GithubIcon, LinkedinIcon } from "./Icons";
+
+function Footer() {
+  return (
+    <>
+      <footer className="w-full min-h-16 p-4 flex items-center justify-center text-white bg-gradient-to-t from-slate-900 to-stone-950">
+        <div className="container mx-auto px-4 flex flex-col items-center justify-center gap-2">
+          <p className="text-center text-sm">Bac à sable - GitHub Repositories</p>
+          <span className="text-sm">{new Date().getFullYear()} Yiroma</span>
+          <div className="flex gap-2">
+            <a
+              href="https://www.linkedin.com/in/yiromaric/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkedinIcon />
+            </a>
+            <a href="https://github.com/Yiroma" target="_blank" rel="noopener noreferrer">
+              <GithubIcon />
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
