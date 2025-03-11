a2df4b041cc3c3dc72618628a9a6a03b9fad4227
Yiroma <yiromaric@gmail.com>
2025-03-11 12:01:23 +0100

    create page RepoPage, link into RepoCard component, update useRepos service

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./pages/Home.tsx";
-
+import RepoPage from "./pages/RepoPage.tsx";
 import App from "./App.tsx";
 
 import "./index.css";
@@ -16,6 +16,10 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+      },
+      {
+        path: "repo/:repoId",
+        element: <RepoPage />,
       },
     ],
   },
