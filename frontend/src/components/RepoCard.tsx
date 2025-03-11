99e37e6d5ea0d32f55996de1373b15cc7fd27638
Yiroma <yiromaric@gmail.com>
2025-03-11 16:03:11 +0100

    create design

--- frontend/src/components/RepoCard.tsx
+++ frontend/src/components/RepoCard.tsx
@@ -8,9 +8,27 @@ type Props = {
 function RepoCard({ repo }: Props) {
   return (
     <>
-      <h2>
-        <Link to={`/repo/${repo.id}`}>{repo.name}</Link>
-      </h2>
+      <Link to={`/repo/${repo.id}`}>
+        <div
+          className="border border-stone-700 rounded-lg p-4 bg-gradient-to-t from-stone-800 to-stone-900 
+     transition-transform duration-300 hover:scale-103 hover:shadow-[0px_4px_20px_rgba(255,255,255,0.1)]"
+        >
+          <div className="flex justify-between items-start">
+            <h2 className="text-lg font-bold">{repo.name}</h2>
+            <span className={`text-sm ${repo.isPrivate ? "text-orange-600" : "text-green-600"}`}>
+              {repo.isPrivate ? "Private" : "Public"}
+            </span>
+          </div>
+          <p className="">{repo.description}</p>
+          <div className="flex flex-wrap gap-2 pt-4">
+            {repo.languages.map((language) => (
+              <span className="text-sm" key={language.node.name}>
+                {language.node.name}
+              </span>
+            ))}
+          </div>
+        </div>
+      </Link>
     </>
   );
 }
